161111f7261f1fa826f75059c05b7269213f77bb
drfrag <drfrag666@hotmail.com>
2020-03-20 14:10:04 +0100

    - We no longer use SVN take 2.

--- src/version.h
+++ src/version.h
@@ -42,15 +42,11 @@
 
 #define DOTVERSIONSTR_NOREV "2.1.8"
 
-// Hack, we no longer use SVN.
-#define SVN_REVISION_STRING "666"
-#define SVN_REVISION_NUMBER 0
-
 // The version string the user actually sees.
-#define DOTVERSIONSTR DOTVERSIONSTR_NOREV " (r" SVN_REVISION_STRING ")"
+#define DOTVERSIONSTR DOTVERSIONSTR_NOREV //" (r" SVN_REVISION_STRING ")"
 
 // The version as seen in the Windows resource
-#define RC_FILEVERSION 2,1,7,SVN_REVISION_NUMBER
+#define RC_FILEVERSION 2,1,8,0//SVN_REVISION_NUMBER
 #define RC_PRODUCTVERSION 2,1,0,0
 #define RC_FILEVERSION2 DOTVERSIONSTR
 #define RC_PRODUCTVERSION2 "2.1"
